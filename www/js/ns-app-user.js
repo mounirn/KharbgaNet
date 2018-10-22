cc21daf689238fc4d285a7cb938292c690551e13
Mounir Nouri <mounir@nouriSolutions.com>
2018-10-22 14:00:32 -0700

    moved user related functions to the new ns-app-user.js module

---
+++ www/js/ns-app-user.js
@@ -0,0 +1,394 @@
+/** NS App Object
+ * Include after: 
+ * - jQuery
+ * - ns-app-utils.js 
+ * - ns-api-client.js
+ */
+if ($ == undefined || $.nsApp == undefined){
+    console.log("Please include jQuery and ns-app-utils before this module");
+    throw new Error("Please include jQuery and ns-app-utils");
+}
+if (nsApiClient == undefined){
+    console.log("Please include ns-api-client before this module");
+    throw new Error("Please include ns-api-client");
+}
+var nsApp = $.nsApp;
+nsApp.init = function(){ 
+   // setup all the various buttons and links events
+   $('#user-state-link').on('click', _refreshUserState);
+   $('#login-link').on('click', onLoginLink);  
+   $('#register-link').on('click', onRegisterLink);
+   $('#login-submit').on('click', onLoginSubmit);
+   $('#register-submit').on('click', onRegisterSubmit);
+   $('#logout-link').on('click', onLogoutSubmit);
+
+    /* We are assuming the UI will have the above links and and a number of panels for:
+    * - login: id login-panel
+    * - register: id=register-panel
+    * - my Account info: id=account-info-panel
+    * Also the followings for messages:
+    * - Welcome message: id=welcome-message
+    * - Account logging status: id="account-message"
+    * In addition the utils message is also used
+    */
+    /**
+     * Handler for login click from UI
+     * @param {any} e
+     */
+    function onLoginLink(e) {
+        e.preventDefault();
+
+     //   var accountTab = $('#main-tabs a[href="#account"]');
+     //   if (accountTab != null &&  accountTab.tab != null &&  accountTab.tab != undefined)
+    //        accountTab.tab('show');  
+
+     //   $('#login-panel').show().removeClass('hidden');
+     //   $('#register-panel').hide().addClass('hidden');
+        transferToLogin();
+    }
+    /**
+     * Handler for register click from UI
+     * @param {any} e
+     */
+    function onRegisterLink(e) {
+        e.preventDefault();
+
+     //   var accountTab = $('#main-tabs a[href="#account"]');
+     //   if (accountTab != null &&  accountTab.tab != null &&  accountTab.tab != undefined)
+     //       accountTab.tab('show');
+
+      //  $('#login-panel').hide().addClass('hidden');
+      //  $('#register-panel').show().removeClass('hidden');
+
+      transferToRegister();
+    }
+
+ 
+    /**
+     * handler for login request 
+     * @param {any} e
+     */
+    function onLoginSubmit(e) {
+        e.preventDefault();
+        var form = $('#login-form');
+
+        // check if the form is valid
+        if (!form.valid()) {
+            displayAccountMessage("Please fix the input errors below.",false);
+            return false;
+        }
+
+        var loginInfo = {
+            LoginID: $('#login-id').val(),
+            Password: $('#login-pwd').val(),
+            RememberMe: $('#login-remember').is(':checked')
+        };
+        displayAccountMessage("Processing... ");
+
+        var result = nsApiClient.userService.validateLogin(loginInfo, function(data, status) {
+            if (data != null) {               
+                displayAccountMessage("Logged in successfully",true);                    
+                console.log(data);
+                setupClientStateWithSession(data.object);           
+                if ($.appViewHandler != null && typeof($.appViewHandler.closeLoginPanel) === 'function')
+                    $.appViewHandler.closeLoginPanel();
+
+                // check the last game 
+               // rejoinLastGameIfAny();
+               transferToMyAccount();
+            }
+            else {
+                setupClientStateWithSession(null);
+            
+                if (status.status === 404 || status.status === 400  )
+                    displayAccountMessage("Invalid user name or password",false);
+                else
+                   displayAccountMessage("Failed to login. Error: " + error.status,false);
+
+                $('#appInfo').html("<pre> " + JSON.stringify(status) + " </pre>");
+            }  
+        });
+    }
+
+    /**
+     * handler for register request 
+     * @param {any} e
+     */
+    function onRegisterSubmit(e) {
+        e.preventDefault();
+        var form = $('#register-form');
+      
+        // check if the form is valid
+        if (!form.valid()) {
+            displayAccountMessage("Please fix the input errors below.",false);
+            return false;
+        }
+
+        var registerInfo = {
+            LoginID: $('#register-login-id').val(),
+            Password: $('#register-pwd').val(),
+            ConfirmPassword: $('#register-pwd-confirm').val(),
+            Name: $('#register-name').val(),
+            Email: $('#register-email').val(),
+            OrgName: $('#register-team').val()
+
+        };
+        displayAccountMessage("Processing...");
+
+        var result = nsApiClient.userService.register(registerInfo, function (data, status) {
+            if (data != null) {
+                $('#appInfo').html(JSON.stringify(data));
+                displayAccountMessage("Registered new account successfully. ",true);
+                console.log(data);
+                setupClientStateWithSession(data.object);      
+                transferToMyAccount();
+                if ($.appViewHandler != null && typeof($.appViewHandler.closeRegisterPanel) === 'function')
+                    $.appViewHandler.closeRegisterPanel();
+            }
+            else {
+                setupClientStateWithSession(null);
+               
+                if (status.status === 404 || status.status === 400)
+                    $('#account-message').html("<div class='alert alert-danger'>Invalid registration info. Errors: " + status.responseText+ " </div>");
+                else
+                    $('#account-message').html("<div class='alert alert-danger'> Failed to register.</div>");
+
+                $('#appInfo').html("<div class='panel panel-danger'> <pre> " + JSON.stringify(status) + " </pre> </div>");
+            }
+        });
+    }
+
+    function transferToMyAccount(){
+        document.location= "../html/my-account.html";
+    }
+    function transferToLogin(){
+        document.location= "../html/login.html";
+    }
+    function transferToRegister(){
+        document.location= "../html/register.html";
+    }
+    /**
+    * handler for logout request
+    * @param {any} e
+    */
+    function onLogoutSubmit(e) {
+        e.preventDefault();
+        if ($.nsApp.state.sessionId == null ||$.nsApp.state.sessionId.length < 10)
+        {
+            displayAccountMessage("invalid session");
+            transferToMyAccount();
+            return; 
+        }
+        displayAccountMessage("Processing...");
+        // add call for back-end to delete the session
+        nsApiClient.userService.logout($.nsApp.state.sessionId, function (data,status) {
+            if (data != null ) {   
+                setupClientStateWithSession(null);
+                transferToLogin(); 
+                displayAccountMessage("Logged out successfully");
+            }
+            else {
+                setupClientStateWithSession(null);
+                //e;
+               
+      
+                displayAccountMessage("Failed to logout.");
+                transferToMyAccount();
+            } 
+        });
+    }
+
+
+   function _refreshUserState(e){
+        if (e!= null)
+            e.preventDefault();
+
+        nsApp.dumpObjectInfo(user,'user-state-table',true);         
+    }
+
+    function setupFormsValidation() {
+        $('#login-form').validate();
+        $('#register-form').validate();
+    }
+
+    /**
+     * @summary Sets up the dropdown list for a the teams field
+     */
+    function setupTeamsHtml5Combobox(){
+        $("#register-team").on('keyup', function () {
+            var val = this.value;
+            if (val.length < 3)
+                return;
+            var result = nsApiClient.clientService.getClients("", val, function (data, status) {
+                if (data != null) {
+                    //   $('#appInfo').html(JSON.stringify(data));
+                    $("#register-team-list").empty();
+                    $.each(data, function () {
+                        // if (this.Status == 0 || this.Status = 1)
+                        $("#register-team-list").append("<option id=client_'" + this.systemId + "' value='" + this.name + "' ></option>");
+                    });
+                }
+                else {
+                    $('#appInfo').html("<div class='alert alert-danger'> <pre> " + JSON.stringify(status) + " </pre> </div>");
+                }
+            });
+        });
+    }
+ 
+    function displayAccountMessage(message, success){
+        if (success == undefined){
+            $('#account-message').html("<div class='alert alert-info'>" + message + "</div>");
+            nsApp.displayInfoMessage(message);
+        }else if (success === true){
+            $('#account-message').html("<div class='alert alert-success'>" + message + "</div>");
+            nsApp.displaySuccessMessage(message);
+        }
+        else if (success === false){
+            $('#account-message').html("<div class='alert alert-danger'>" + message + "</div>");
+            nsApp.displayErrorMessage(message);
+        }else{
+            $('#account-message').html("<div class='alert alert-warning'>" + message + " - " + success+ "</div>"); 
+            nsApp.displayInfoMessage(message + " - " + success);
+        }   
+    }           
+
+    /**
+     * @summary checks the session coockie
+     */
+    function checkSessionCookie() {
+        var cookie = nsApp.getCookie(C_NSSID);
+        if (typeof(cookie) === "string" && cookie.length > 10)
+            checkSession(cookie);       
+        else{
+            // check local storage
+            if (window.localStorage != null){
+                var sid = window.localStorage.getItem(C_NSSID);
+                if (typeof sid  === "string" && sid.length > 10)
+                    checkSession(sid);   
+                else{
+                    setupClientStateWithSession(null);  
+                }
+            }
+            else{
+                setupClientStateWithSession(null);
+            }
+        }
+    }
+
+    /**
+     * checks a given session with the back end and update the UI accordingly
+     * @param {any} sessionId the session id
+     */
+    function checkSession(sessionId) {
+        displayAccountMessage("Processing...");
+        var result = nsApiClient.userService.checkSession(sessionId, function (data, status) {
+            if (data != null) {
+                $('#appInfo').html(JSON.stringify(data));
+                displayAccountMessage("");
+
+                var session = data.object;
+                
+                if (session != null) {
+                    setupClientStateWithSession(session);
+                    
+                }
+                else {
+                    setupClientStateWithSession(null);
+                }
+
+
+            }
+            else {
+                nsApp.setCookie(C_NSSID, "");
+                nsApp.state.loggedIn = false;
+                setupMyAccount();
+                nsApp.state.sessionId = "";
+                nsApp.state.session = null;
+                nsApp.user.name = "";
+
+
+                if (status.status === 404 || status.status === 400)
+                    displayAccountMessage("Invalid Session - Please Login", false);
+                else
+                    displayAccountMessage("Failed to access the system. Please try your request again later.",false);
+
+            }
+        });
+    }
+
+    /**
+     * @summary Sets up the MyAccount tab based on the current app client state
+     */
+    function setupMyAccount() {
+        if (nsApp.state.loggedIn === true) {
+         //   $('#account-info-panel').show().removeClass('hidden');
+            $('#account-welcome').show().removeClass('hidden');
+            $('#account-welcome').html("<strong> Welcome " + nsApp.user.name + "</strong>");
+            $('#login-li').hide().addClass('hidden');
+            $('#register-li').hide().addClass('hidden');
+            $('#logout-li').show().removeClass('hidden');
+        //    $('#login-panel').hide().addClass('hidden');
+        //    $('#register-panel').hide().addClass('hidden');
+            displayAccountMessage("Signed In");
+        
+
+        } else {
+         //   $('#login-panel').show().removeClass('hidden');
+         //   $('#register-panel').hide().addClass('hidden');
+         //   $('#account-info-panel').hide().addClass('hidden');
+            $('#account-welcome').hide().addClass('hidden');
+            $('#login-li').show().removeClass('hidden');
+            $('#register-li').show().removeClass('hidden');
+            $('#logout-li').hide().addClass('hidden');
+            displayAccountMessage("Signed Out");
+        }
+        $('#account-name').text(nsApp.user.name);
+      //  $('#account-org-id').text(nsApp.state.session.clientId);
+      //  $('#account-session-id').text(nsApp.state.sessionId);
+    
+    }
+
+    /**
+    * @summary sets up the client state with the given session
+    * @param {any} session - the session info
+    */
+    function setupClientStateWithSession(session) {
+        if (session != null) {
+            nsApp.state.session = session;
+            nsApp.state.sessionId = session.sessionId;
+            nsApp.user.name = session.fullName;
+            nsApp.state.loggedIn = session.isActive;
+            nsApp.setCookie(C_NSSID, session.sessionId);
+        }
+        else {
+            nsApp.state.session = null;
+            nsApp.state.sessionId = "";
+            nsApp.user.name = "";
+            nsApp.state.loggedIn = false;
+            nsApp.setCookie(C_NSSID, "");
+        }
+        setupMyAccount();
+    }
+
+ 
+    setupMyAccount();
+    setupFormsValidation();
+
+ 
+
+    this.setSessionId = function (sid) {
+        nsApp.setCookie(C_NSSID, sid);
+        if (sid != "")
+            checkSessionCookie();  // check it and update state
+    };
+
+    /** 
+     * @summary checks the session cookie, setup 
+     * the form with any data  
+     */ 
+    this.setup = function () {
+        checkSessionCookie();
+        setupTeamsHtml5Combobox();
+    };
+};
+nsApp.init(); // init 
--- www/js/ns-app-utils.js
+++ www/js/ns-app-utils.js
@@ -1,52 +1,209 @@
-﻿function log(message)   {
-    
-    try     {
-        console.log(message);
-    }
-    catch(err) { 
-        //no action. probably just IE
-    }
-}
-/**
-   * Helper function for reading cookie
-   * @param {any} key - a key
-   * @returns {any} - the value or null
-   */
-function getCookie(key) {
-    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-    return keyValue ? keyValue[2] : null;
+﻿if ($ == undefined ){
+    console.log("Please include jQuery v1.10 or higher before this module");
+    throw new Error("Please include jQuery");
 }
 
-/**
- * Helper function for setting cookie
- * @param {any} key - the key to set
- * @param {any} value - the value
- */
-function setCookie(key, value) {
-    var expires = new Date();
-    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
-    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-}
-
-var cw;
-
-function startWorker() {
-    if (typeof (Worker) !== "undefined") {
-        if (typeof (cw) == "undefined") {
-            cw = new Worker("kharbga-computer.js");
+function NSApp (){
+    this.state= {// the client app state
+        session: {
+            id: "",
+            clientId: "",
         }
-        cw.onmessage = function (event) {
-            console.log(event.data);
-        };
-        cw.onerror = function (event) {
-            console.log(event.data);
-        };
-    } else {
-        console.log("Sorry! No Web Worker support.");
-    }
-}
+    };  
+    this.user = {// the current user info
+        name: "Guest"
+    };  
+    
 
-function stopWorker() {
-    cw.terminate();
-    cw = undefined;
+    this.log= function(message) {
+        try     {
+            console.log(message);
+        }
+        catch(err) { 
+            //no action. probably just IE
+        }
+    };
+
+
+    /**
+     * Helper function for reading cookie
+     * @param {any} key - a key
+     * @returns {any} - the value or null
+     */
+    this.getCookie = function(key) {
+        var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+        var ret = keyValue ? keyValue[2] : null;
+        if (ret === null){
+            if (window.localStorage != null){
+                val = window.localStorage.getItem(key);            
+            }
+        }
+        return ret;
+    };
+
+    /**
+     * Helper function for setting cookie
+     * @param {any} key - the key to set
+     * @param {any} value - the value
+     */
+    this.setCookie = function(key, value) {
+        var expires = new Date();
+        expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+        document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+
+        // store in local storage 
+        if (window.localStorage != null){
+            window.localStorage.setItem(key, value);            
+        }
+    };
+
+
+    this.cw = null;
+    this.startWorker = function() {
+        if (typeof (Worker) !== "undefined") {
+            if (this.cw == null) {
+                this.cw = new Worker("kharbga-computer.js");
+            }
+            this.cw.onmessage = function (event) {
+                console.log(event.data);
+            };
+            this.cw.onerror = function (event) {
+                console.log(event.data);
+            };
+        } else {
+            console.log("Sorry! No Web Worker support.");
+        }
+    };
+
+    this.stopWorker = function() {
+        this.cw.terminate();
+        this.cw = undefined;
+    };
+
+        
+    /**
+     * @summary Displays an error message to the user
+     * @param {string} message - the message to display
+     */
+    this.displayErrorMessage= function(message){
+        $('#message').html("<div class='alert alert-danger'>" + message + "</div>");
+    };
+
+    /**
+     * @summary Displays a success message to the user
+     * @param {string} message - the message to display
+     */
+    this.displaySuccessMessage= function(message){
+        $('#message').html("<div class='alert alert-success'>" + message + "</div>");
+    };
+
+    /**
+     * @summary Displays a message about the game 
+     * @param {string} message - the message to display
+     */
+    this.displayGameMessage= function(message){
+        $('#game-message').html("<div class='alert alert-info'>" + message + "</div>");
+    };
+        /**
+     * @summary Displays a message status change from the messaging server
+     * @param {string} message - the message to display
+     */
+    this.displayNetMessage= function(message){
+        $('#net-message').html("<div class='alert alert-info'>" + message + "</div>");
+        //   $('#message').html("<div class='alert alert-info'>" + message + "</div>");
+        $('#main-message').html("<div class='alert alert-info'>" + message + "</div>");
+    };
+
+    /**
+     * @summary Display computer Message
+     * @param {string} message - the message to display
+     */
+    this.displayComputerMessage= function(message){
+        $('#computer-message').html("<div class='alert alert-info'>" + message + "</div>");
+    };
+
+    /**
+     * @summary Displays an informational message to the user
+     * @param {string} message - the message to display
+     */
+    this.displayInfoMessage= function(message){
+        $('#message').html("<div class='alert alert-info'>" + message + "</div>");
+    };
+        
+    /**
+     * @summary Displays a warning message to the user
+     * @param {string} message - the message to display
+     */
+    this.displayWarningMessage = function (message){
+        $('#message').html("<div class='alert alert-warning'>" + message + "</div>");
+    };
+
+    /**
+     * @summary outputs and object properties to the table body element  
+     * @param {any} data  - an object
+     * @param {string} elementId - the id of the element table to output the object data
+     *  in the body. the element body should by in html as '{elementId} + '-body'
+     * @param {boolean} clear - clear the previous data or not
+     */
+    this.dumpObjectInfo = function(data, elementId, clear){ 
+    //   $('#main-message').html("<div class='alert alert-info'>Processing...</div>"); 
+        if (clear === true){
+            $('#' + elementId +'-body').html(""); 
+        }
+
+        Object.keys(data).forEach( function(key){
+            var obj = data[key]; 
+            var objType = typeof(obj);
+            if (objType == "object" ){               
+                var tr = "<tr><th style='width:20%'>"+this.toDisplayString(key) + ":</th><td>" ;
+                var objHtml = "<ul>";
+                
+                if (obj!= null){
+                    Object.keys(obj).forEach( function(key2){
+                        if (typeof obj[key2] != "function" )
+                        {                 
+                            var li = "<li>" + this.toDisplayString(key2) + ": " + obj[key2]+"</li>";
+                            objHtml +=(li);
+                        }
+                    });
+                } 
+                objHtml += "<ul>";
+                tr += objHtml;
+                tr += "</td></tr>";
+
+                $('#' + elementId +'-body').append(tr);
+            }
+            else if (objType == "function" ){
+                // skip
+            }
+            // check if array
+            else{
+                var tr2 = "<tr><th style='width:20%'>"+this.toDisplayString(key) + ":</th><td>" + obj+"</td></tr>";
+                $('#' + elementId +'-body').append(tr2);
+            }
+        });  
+    };
+
+    
+    /** 
+     * @summary Converts a camel case string to display 
+     *  @returns {string} the converted string
+     */
+    this.toDisplayString = function(key){
+        // key.replace(/([A-Z])/g, function($1){return " "+$1.toLowerCase();});
+        // insert a space between lower & upper
+        var ret = key.replace(/([a-z])([A-Z])/g, '$1 $2')
+        // space before last upper in a sequence followed by lower
+        .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+        // uppercase the first character
+        .replace(/^./, function(str){ return str.toUpperCase();}); 
+
+        return ret;
+    };
 }
+$.nsApp = new NSApp();
+var nsApp = $.nsApp;  
+nsApp.C_NSGID = "_nsgid";
+nsApp.C_NSSID = "_nssid";
+
+   
